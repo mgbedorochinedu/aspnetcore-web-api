5bef0509ebe9a6819590ee7bf9524a05dfc3280c
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2021-08-19 13:57:16 -1200

    Implement Get Book With Authors By Id

--- my-books/Data/Services/BooksService.cs
+++ my-books/Data/Services/BooksService.cs
@@ -50,7 +50,23 @@ namespace my_books.Data.Services
         public List<Book> GetAllBooks() => _context.Books.ToList();
 
         //Get Book By Id Method
-        public Book GetBookById(int bookId) => _context.Books.FirstOrDefault(n => n.Id == bookId);
+        public BookWithAuthorsVM GetBookById(int bookId)
+        {
+            var _bookWithAuthors = _context.Books.Where(n => n.Id == bookId).Select(book => new BookWithAuthorsVM()
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead.Value : null,
+                Rate = book.IsRead ? book.Rate.Value : null,
+                Genre = book.Genre,
+                CoverUrl = book.CoverUrl,
+                PublisherName = book.Publisher.Name,
+                AuthorNames = book.Book_Authors.Select(n => n.Author.FullName).ToList()
+            }).FirstOrDefault();
+
+            return _bookWithAuthors;
+        }
 
         //Update Book Method
         public Book UpdateBookById(int bookId, BookVM book)
