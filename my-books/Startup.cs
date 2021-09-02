f4903b61ec3f6cea1b78e4cbcffa2134d4742c82
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2021-09-01 15:42:55 -1200

    Implemented web Api versioning

--- my-books/Startup.cs
+++ my-books/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -42,6 +43,18 @@ namespace my_books
             services.AddTransient<BooksService>();
             services.AddTransient<AuthorsService>();
             services.AddTransient<PublishersService>();
+
+            services.AddApiVersioning(config =>
+            {
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+
+                //This Config supports HTTP Header-Based Versioning
+                //config.ApiVersionReader = new HeaderApiVersionReader("custom-version-header");
+
+                //This Config the HTTP Media Type-Based Versioning
+                //config.ApiVersionReader = new MediaTypeApiVersionReader();
+            });
 
             services.AddSwaggerGen(c =>
             {
