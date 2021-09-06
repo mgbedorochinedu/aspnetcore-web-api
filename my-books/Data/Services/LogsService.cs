0e0c88c76869631a5f22203d188844b5123ba4ef
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2021-09-05 18:24:07 -1200

    Retrieving all logs from the database

---
+++ my-books/Data/Services/LogsService.cs
@@ -0,0 +1,20 @@
+﻿using my_books.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Data.Services
+{
+    public class LogsService
+    {
+        private readonly AppDbContext _context;
+
+        public LogsService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Log> GetAllLogsFromDB() => _context.Logs.ToList();
+    }
+}
